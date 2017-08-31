6f882124cc0e57eeea1941bc9fcb76045a51978d
America Calleros <america.quintero@gmail.com>
2017-08-31 18:14:02 -0500

    Raising errors is overkill not very user friendly for this.

--- spec/file_indexer_spec.rb
+++ spec/file_indexer_spec.rb
@@ -6,12 +6,11 @@ describe "File Indexer" do
   let(:empty_text_file) { "test_files/EmptyFile.txt" }
 
   describe "#verify_input" do
-    it "does not raise error if input file provided" do
-      expect{verify_input([text_file_1])}.not_to raise_error
+    it "returns false if no input file provided" do
+      expect(verify_input([])).to eq false
     end
-
-    it "can accept multiple files" do
-      expect{verify_input([text_file_1, text_file_2, text_file_3])}.not_to raise_error
+    it "returns true if input file provided" do
+      expect(verify_input([text_file_1])).to be true
     end
   end
 
@@ -26,13 +25,17 @@ describe "File Indexer" do
   end
 
   describe "#sort_by_count" do
+    it "returns false if file is empty" do
+      expect(sort_by_count()).to be false
+    end
+
     it "returns a data hash" do
       word_hash = read_file_data(text_file_1)
 
       expect(sort_by_count(word_hash)).to be_a(Array)
     end
 
-    it "sorted hash should be same size as original" do
+    it "sorted array should be same size as original" do
       word_hash = read_file_data(text_file_1)
       sorted_array = sort_by_count(word_hash)
 
@@ -41,12 +44,20 @@ describe "File Indexer" do
   end
 
   describe "#display_results" do
-    it "prints 10 results" do
+    it "prints 10 results if results are 10 or more" do
       sample_array = Array.new
       %w(toes my for scared I'm cold get will it and coming is winter ).each_with_index do |string, index|
 	sample_array << [string, index]
       end
       expect( display_results(sample_array)).to eq(10)
+    end
+
+    it "prints 5 results if only 5 results" do
+      sample_array = Array.new
+      %w(files funny of case in ).each_with_index do |string, index|
+	sample_array << [string, index]
+      end
+      expect( display_results(sample_array)).to eq(5)
     end
   end
 end
