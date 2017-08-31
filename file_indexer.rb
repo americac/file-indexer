834487e894f7a12abdec0225df97f627c7b49a99
America Calleros <america.quintero@gmail.com>
2017-08-30 22:56:20 -0500

    Not symbols, check if file is empty

--- file_indexer.rb
+++ file_indexer.rb
@@ -2,6 +2,7 @@
 
 def verify_input_file(input_file=nil)
   raise "Input file is required." if input_file.nil?
+  raise "Input file is empty." if File.zero?(input_file.to_s)
 end
 
 input_file = ARGV[0] ? ARGV[0] : nil
--- spec/file_indexer_spec.rb
+++ spec/file_indexer_spec.rb
@@ -1,14 +1,19 @@
 require_relative "../file_indexer"
 describe "File Indexer" do
   let(:text_file_1) { "test_files/FannyLambert.txt" }
+  let(:empty_text_file) { "test_files/EmptyFile.txt" }
 
   describe "#verify_input_file" do
     it "raises error if no argument set"do
       expect{verify_input_file()}.to raise_error(RuntimeError)
     end
 
+    it "raises error if file is empty"do
+      expect{verify_input_file(empty_text_file)}.to raise_error(RuntimeError)
+    end
     it "does not raise error if input file provided" do
-      expect{verify_input_file(:text_file_1)}.not_to raise_error
+      expect{verify_input_file(text_file_1)}.not_to raise_error
     end
   end
+
 end
