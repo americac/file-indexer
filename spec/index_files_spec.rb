0714f93e8738273f6dcd722c5910a2adb6f2ea64
America Calleros <america.quintero@gmail.com>
2017-08-31 22:52:49 -0500

    Test executing script as well

--- spec/index_files_spec.rb
+++ spec/index_files_spec.rb
@@ -1,13 +1,19 @@
 require_relative "../index_files"
 describe "index_files.rb" do
   let(:text_file_1) { "test_files/FannyLambert.txt" }
-  let(:funny_file) { "test_files/FunnyFile.txt" }
-  let(:empty_text_file) { "test_files/EmptyFile.txt" }
 
   describe "#read_file_data" do
     it "displays usage if no arguments are passed" do
       expect{verify_input([])}.to output("Hey! At least one file needs to be passed as a argument:\n./index_files.rb file_name_1 file_name_2\n\n").to_stdout
 
+    end
+
+    it "returns false if no arguments are passed" do
+      expect(verify_input([])).to be false
+    end
+
+    it "returns true if argument is passed" do
+      expect(verify_input([text_file_1])).to be true
     end
   end
 end
